9efe4bb75b7b51ddb8a9a68ccde03616958aa8a2
saturday <hmnth07@gmail.com>
2020-09-06 11:54:17 +0530

    Added persons name

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ var previousButton = document.getElementById('previousButton');
 var nextButton = document.getElementById('nextButton');
 
 var review = document.getElementById("review");
-var name = document.getElementById("name");
+var personName = document.getElementById("personName");
 var position = document.getElementById("position");
-var profileimage = document.getElementById("profileimage");
+var profileImage = document.getElementById("profileImage");
 
 nextButton.addEventListener('click', function() {
     review.innerHTML = '"If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer."';
-    name.innerHTML = "John Tarkpor";
+    personName.innerHTML = "John Tarkpor";
     position.innerHTML = " Junior Front-end Developer";
-    profileimage.src = "images/image-john.jpg";
+    profileImage.src = "images/image-john.jpg";
 });
 
 previousButton.addEventListener('click', function() {
     review.innerHTML = '"I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough I’m now in the job of my dreams and so excited about the future."';
-    name.innerHTML = "Tanya Sinclair";
+    personName.innerHTML = "Tanya Sinclair";
     position.innerHTML = " UX Engineer";
-    profileimage.src = "images/image-tanya.jpg";
+    profileImage.src = "images/image-tanya.jpg";
 });
 
 
